d6b9207956ebb5bb812d9e70e96b6fa9da4cb892
ebommes <Elisabeth.bommes@hu-berlin.de>
2015-08-12 16:24:24 +0200

    Updated Keywords, "See also", corrected typo, style
    Additional keywords: density, standard normal, random-number-generation, univariate, gauss, gaussian, continuous
    See also: SFElognormal, MVAtdis, MVAgausscauchy
    Typo: funtion -> function
    Style: Added blanks after commas as well as before and after equal signs

--- MSMpdfnormal.R
+++ MSMpdfnormal.R
@@ -4,23 +4,24 @@
 # Quantlet:     MSMpdfnormal
 # ------------------------------------------------------------------------------
 # Description:  Simulates data from the standard normal distribution N(0,1) 
-#               and then plots the PDF funtion.
+#               and then plots the PDF function.
 # ------------------------------------------------------------------------------
-# Keywords:     simulation, pdf, plot, normal, standard, normal-distribution, distribution
+# Keywords:     simulation, pdf, plot, normal, standard, normal-distribution, distribution, density, standard normal,
+#               random-number-generation, univariate, gauss, gaussian, continuous
 # ------------------------------------------------------------------------------ 
-# See also:     MSMcdfnormal, MSMpdfbinomial    
+# See also:     MSMcdfnormal, MSMpdfbinomial, SFElognormal, MVAtdis, MVAgausscauchy
 # ------------------------------------------------------------------------------
 # Author:       Xiu Xu 20150603
 # ------------------------------------------------------------------------------
 
 
 graphics.off()
-rm(list = ls()) #remove variable definitions
+rm(list = ls()) # remove variable definitions
 
 n = 100
 set.seed(80)
-y = rnorm(n,0,1)  #Generate standard normal random numbers
+y = rnorm(n, 0, 1)  # Generate standard normal random numbers
 
-#Plot the PDF 
-plot(seq(-4,4,0.2), dnorm(seq(-4,4,0.2),0,1), col="blue", type="l", lwd=2.5, ylab="PDF", xlab="X")
-abline(v=0)
+# Plot the PDF 
+plot(seq(-4, 4, 0.2), dnorm(seq(-4, 4, 0.2), 0, 1), col = "blue", type = "l", lwd = 2.5, ylab = "PDF", xlab = "X")
+abline(v = 0)
