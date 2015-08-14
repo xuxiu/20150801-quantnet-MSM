60a16befeb0201d865ea0a177ee9761aaed16c22
xuxiu <xiu.xu@hu-berlin.de>
2015-08-14 09:54:56 +0200

    MSMpdfmultinormal: Header, coding style

--- MSMpdfmultinormal.R
+++ MSMpdfmultinormal.R
@@ -1,40 +1,33 @@
 # ------------------------------------------------------------------------------
-# Course: MSM - Selected Topics of Mathematical Statistics
+# Course:      MSM - Selected Topics of Mathematical Statistics
 # ------------------------------------------------------------------------------
-# Quantlet: MSMpdfmultinormal
+# Quantlet:    MSMpdfmultinormal
 # ------------------------------------------------------------------------------
-# Description: plot multimornal density function 
+# Description: Simulate 1000 data point from the binormal distribution with mean value (0, 0) 
+#              and covariance matrix c(1, 0.5, 0.5, 1) and then plot the multinormal PDF function.
 # ------------------------------------------------------------------------------
-# Usage: 
-# ------------------------------------------------------------------------------
-# Inputs: 
-# ------------------------------------------------------------------------------
-# Output: 
-# ------------------------------------------------------------------------------
-# Keywords:  multinormal density    
+# Keywords:    multivariate normal, simulation, pdf, plot, bivariate, density, normal, normal-distribution,
+#              3D, visualization, distribution, estimation
 # ------------------------------------------------------------------------------ 
-# See also:  
+# See also:    MSMcdfnormal, MSMpdfbinomial, SPM2density2D£¬SPMcontour2D,SPMdensity 
 # ------------------------------------------------------------------------------
-# Author:    Xiu Xu 20150603
+# Author:      Xiu Xu 20150603
 # ------------------------------------------------------------------------------
 
 graphics.off()
-rm(list=ls()) #remove variable definitions
+rm(list = ls()) #remove variable definitions
 
-#Load required packages
-library(MASS)
+# install and load packages
+libraries = c("MASS")
+lapply(libraries, function(x) if (!(x %in% installed.packages())) {
+  install.packages(x)
+})
+lapply(libraries, library, quietly = TRUE, character.only = TRUE)
 
-getwd()
-setwd("")
+bivn = mvrnorm(1000, mu = c(0, 0), Sigma = matrix(c(1, .5, .5, 1), 2))
 
-bivn <- mvrnorm(1000, mu = c(0, 0), Sigma = matrix(c(1, .5, .5, 1), 2))
+# Kernel density estimate
+bivn.kde = kde2d(bivn[, 1], bivn[, 2], n = 50)
 
-# kernel density estimate
-bivn.kde <- kde2d(bivn[,1], bivn[,2], n = 50)
-
-# plot your results
-name=paste("Fig_MSMpdfmultinormal",".pdf",sep="")
-pdf(name)
-persp(bivn.kde, phi = 45, theta = 30, xlab="X1", ylab="X2", zlab="PDF")
-dev.off()
-
+# Plot results
+persp(bivn.kde, phi = 45, theta = 30, xlab = "X1", ylab = "X2", zlab = "PDF")
